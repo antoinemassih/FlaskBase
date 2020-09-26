4d293667ef7bd6aa290c848617ffe21643e22a0d
antoinemassih <Monkeyhero69>
2020-09-26 15:03:01 +0400

    added basic Routes

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return "hello"
 
 
-@app.route('/POST/')
-def post_ul():
-    return 'this is a post'
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 
-@app.route('/GET/')
-def get_url():
-    return 'this is a get'
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
 
 
 if __name__ == '__main__':
